63fb6641d0e392e09a26ee30b67c4b61dcdf2406
richard <richard@vdberg.org>
2020-11-15 22:11:12 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+# evohome-exporter
+
+Honeywell Evohome data exporter for prometheus
+
+Uses the python evohomeclient from:
+https://pypi.org/project/evohomeclient/
+https://github.com/watchforstock/evohome-client
---
+++ evohome-exporter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+import time
+from requests import HTTPError
+from evohomeclient2 import EvohomeClient
+from keys import username, password
+import prometheus_client as prom
+
+timeout=60
+
+def refreshEvohome(myclient):
+  try:
+    myclient._login()
+  except HTTPError as e:
+    print('HTTPError: {}'.format(str(e)), file=sys.stderr)
+    return False
+  return time.time()
+
+if __name__ == '__main__':
+  eht = prom.Gauge('evohome_temperature_celcius', 'Evohome temperatuur in celsius', ['name', 'thermostat', 'id', 'type'])
+  upd = prom.Gauge('evohome_updated'            , 'Evohome client last updated')
+  up  = prom.Gauge('evohome_up'                 , 'Evohome client status')
+  prom.start_http_server(8082)
+  client = EvohomeClient(username, password)
+  updated = time.time()
+
+  while True:
+    if updated:
+      up.set(1)
+      upd.set(updated)
+      for d in client.temperatures():
+        eht.labels(d['name'],d['thermostat'],d['id'],'measured').set(d['temp'])
+        eht.labels(d['name'],d['thermostat'],d['id'],'setpoint').set(d['setpoint'])
+    else:
+      up.set(0)
+    time.sleep(timeout)
+    updated = refreshEvohome(client)
