aaa5fb68fdd574c2a02ac99b15ab23fead0c7a70
Richard van den Berg <richard@vdberg.org>
2020-12-06 23:17:29 +0100

    Black formatting

--- evohome-exporter.py
+++ evohome-exporter.py
@@ -6,54 +6,69 @@ from evohomeclient2 import EvohomeClient
 from keys import username, password
 import prometheus_client as prom
 
-timeout=60
+timeout = 60
+
 
 def loginEvohome(myclient):
-  try:
-    myclient._login()
-  except Exception as e:
-    print('{}: {}'.format(type(e).__name__, str(e)), file=sys.stderr)
-    return False
-  return True
-
-if __name__ == '__main__':
-  eht = prom.Gauge('evohome_temperature_celcius', 'Evohome temperatuur in celsius', ['name', 'thermostat', 'id', 'type'])
-  upd = prom.Gauge('evohome_updated'            , 'Evohome client last updated')
-  up  = prom.Gauge('evohome_up'                 , 'Evohome client status')
-  prom.start_http_server(8082)
-  try:
-    client = EvohomeClient(username, password)
-  except Exception as e:
-    print('ERROR: can\'t create EvohomeClient\n{}: {}'.format(type(e).__name__, str(e)), file=sys.stderr)
-    sys.exit(1)
-  loggedin = True
-  lastupdated = 0
-
-  while True:
-    temps = []
     try:
-      temp = client.temperatures()
-      for t in temp:
-        temps.append(t)
-      updated = True
-      lastupdated = time.time()
+        myclient._login()
     except Exception as e:
-      print('{}: {}'.format(type(e).__name__, str(e)), file=sys.stderr)
-      temps = {}
-      updated = False
-      loggedin=loginEvohome(client)
-      if loggedin:
-        continue
+        print("{}: {}".format(type(e).__name__, str(e)), file=sys.stderr)
+        return False
+    return True
 
-    if loggedin and updated:
-      up.set(1)
-      upd.set(lastupdated)
-      for d in temps:
-        if d['temp']:
-          eht.labels(d['name'],d['thermostat'],d['id'],'measured').set(d['temp'])
-        if d['setpoint']:
-          eht.labels(d['name'],d['thermostat'],d['id'],'setpoint').set(d['setpoint'])
-    else:
-      up.set(0)
 
-    time.sleep(timeout)
+if __name__ == "__main__":
+    eht = prom.Gauge(
+        "evohome_temperature_celcius",
+        "Evohome temperatuur in celsius",
+        ["name", "thermostat", "id", "type"],
+    )
+    upd = prom.Gauge("evohome_updated", "Evohome client last updated")
+    up = prom.Gauge("evohome_up", "Evohome client status")
+    prom.start_http_server(8082)
+    try:
+        client = EvohomeClient(username, password)
+    except Exception as e:
+        print(
+            "ERROR: can't create EvohomeClient\n{}: {}".format(
+                type(e).__name__, str(e)
+            ),
+            file=sys.stderr,
+        )
+        sys.exit(1)
+    loggedin = True
+    lastupdated = 0
+
+    while True:
+        temps = []
+        try:
+            temp = client.temperatures()
+            for t in temp:
+                temps.append(t)
+            updated = True
+            lastupdated = time.time()
+        except Exception as e:
+            print("{}: {}".format(type(e).__name__, str(e)), file=sys.stderr)
+            temps = {}
+            updated = False
+            loggedin = loginEvohome(client)
+            if loggedin:
+                continue
+
+        if loggedin and updated:
+            up.set(1)
+            upd.set(lastupdated)
+            for d in temps:
+                if d["temp"]:
+                    eht.labels(d["name"], d["thermostat"], d["id"], "measured").set(
+                        d["temp"]
+                    )
+                if d["setpoint"]:
+                    eht.labels(d["name"], d["thermostat"], d["id"], "setpoint").set(
+                        d["setpoint"]
+                    )
+        else:
+            up.set(0)
+
+        time.sleep(timeout)
